6e7ae399fd6efa0f9651c91a50b89ea77f2981ed
This-swapnil <sswapnil0098@gmail.com>
2025-05-10 18:59:19 +0530

    Account Registration Page Locators and Actions Methods Created

---
+++ src/test/java/pageObject/AccountRegistrationPage.java
@@ -0,0 +1,68 @@
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class AccountRegistrationPage extends BasePage {
+
+    //Locators
+    @FindBy(xpath = "//input[@id='input-firstname']")
+    WebElement txtFirstName;
+
+    @FindBy(xpath = "//input[@id='input-lastname']")
+    WebElement txtLastName;
+
+    @FindBy(xpath = "//input[@id='input-email']")
+    WebElement txtEmail;
+
+    @FindBy(xpath = "//input[@id='input-password']")
+    WebElement txtPassword;
+
+    @FindBy(xpath = "//input[@name='agree']")
+    WebElement chkdPolicy;
+
+    @FindBy(xpath = "//button[normalize-space()='Continue']")
+    WebElement btnClick;
+
+    @FindBy(xpath = "//h1[normalize-space()='Your Account Has Been Created!']")
+    WebElement msgConfirmation;
+
+    public AccountRegistrationPage(WebDriver driver) {
+        super(driver);
+    }
+
+    //Action Methods
+    public void setFirstName(String fname) {
+        txtFirstName.sendKeys(fname);
+    }
+
+    public void setLastName(String lname) {
+        txtLastName.sendKeys(lname);
+    }
+
+    public void setEmail(String email) {
+        txtEmail.sendKeys(email);
+    }
+
+    public void setPassword(String pwd) {
+        txtPassword.sendKeys(pwd);
+    }
+
+    public void setPrivacyPolicy() {
+        wait.until(ExpectedConditions.elementToBeClickable(chkdPolicy)).click();
+    }
+
+    public void clickSubmit() {
+        btnClick.click();
+    }
+
+    public String getConfirmationMsg() {
+        try {
+            return msgConfirmation.getText();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+}
